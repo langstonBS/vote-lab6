edc3a57ef0522839ea724e551fc7699e621a2265
langstonBS <langston.beckwithstanley@gmail.com>
2020-06-16 18:05:39 -0700

    finised the poll routs

--- __tests__/polls.test.js
+++ __tests__/polls.test.js
@@ -7,11 +7,12 @@ const request = require('supertest');
 const app = require('../lib/app');
 const Organization = require('../lib/models/Organization');
 const Poll = require('../lib/models/Poll');
+const User = require('../lib/models/User');
 
 
 
 describe('POLLS routes', () => {
-  beforeAll(async () => {
+  beforeAll(async() => {
     const uri = await mongod.getUri();
     return connect(uri);
   });
@@ -21,7 +22,7 @@ describe('POLLS routes', () => {
   });
 
   let organization;
-  beforeEach(async () => {
+  beforeEach(async() => {
     organization = await Organization.create({
       title: 'Langston Lots',
       description: 'parking lots on blimps so that there is always parking in the sky',
@@ -29,13 +30,33 @@ describe('POLLS routes', () => {
     });
   });
 
-  afterAll(async () => {
+  const agent = request.agent(app);
+
+  let user;
+  beforeEach(async() => {
+    user = await User.create({
+      name: 'langston Thats me',
+      phone: '(555) 555-555',
+      email: 'to personal',
+      communicationMedium: 'phone',
+      imageUrl: 'im an dimmiage',
+      password: '1234'
+    });
+    return agent
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'to personal',
+        password: '1234'
+      });
+  });
+
+  afterAll(async() => {
     await mongoose.connection.close();
     return mongod.stop();
   });
 
-  it('CREATES a poll via POST', async () => {
-    return request(app)
+  it('CREATES a poll via POST', async() => {
+    return agent
       .post('/api/v1/polls')
       .send({
         organization: organization._id,
@@ -49,47 +70,49 @@ describe('POLLS routes', () => {
           organization: organization.id,
           title: 'the poll',
           description: 'its a pole',
-          options: ["1", "2"],
+          options: ['1', '2'],
           __v: 0
         });
       });
   });
 
-  it('GETS a poll via GET', async () => {
-    return Poll.create(
+  it('GETS a poll via GET', async() => {
+    const poll =  Poll.create(
       {
         organization: organization._id,
         title: 'the poll',
         description: 'its a pole',
         options: [1, 2]
-      }
-    )
-      .then(() => request(app).get(`/api/v1/polls`))
+      });
+    
+    return agent
+      .get('/api/v1/polls')
       .then(res => {
         expect(res.body).toEqual([{
           _id: expect.anything(),
           organization: {
             _id: expect.anything(),
-            title: "Langston Lots"
+            title: 'Langston Lots'
           },
           title: 'the poll',
           description: 'its a pole',
-          options: ["1", "2"],
+          options: ['1', '2'],
           __v: 0
         }]);
       });
   });
 
-  it('GETS a single poll via GET ID', async () => {
-    return Poll.create(
+  it('GETS a single poll via GET ID', async() => {
+    const poll = await Poll.create(
       {
         organization: organization._id,
         title: 'the poll',
         description: 'its a pole',
         options: [1, 2]
-      }
-    )
-      .then(poll => request(app).get(`/api/v1/polls/${poll.id}`))
+      });
+
+    return agent
+      .get(`/api/v1/polls/${poll.id}`)
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.anything(),
@@ -100,45 +123,46 @@ describe('POLLS routes', () => {
           },
           title: 'the poll',
           description: 'its a pole',
-          options: ["1", "2"],
+          options: ['1', '2'],
           __v: 0
         });
       });
   });
 
-  it('UPDATES polls vea PATCH', () => {
-    return Poll.create(
+  it('UPDATES polls vea PATCH', async() => {
+    const poll = await Poll.create(
       {
         organization: organization._id,
         title: 'the poll',
         description: 'its a pole',
         options: [1, 2]
-      }
-    )
-      .then(poll => request(app)
-        .patch(`/api/v1/polls/${poll.id}`)
-        .send({ title: 'NEW POLL', description: 'IM ME' }))
+      });
+
+    return agent
+      .patch(`/api/v1/polls/${poll.id}`)
+      .send({ title: 'NEW POLL', description: 'IM ME' })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.anything(),
           organization: organization.id,
           title: 'NEW POLL',
           description: 'IM ME',
-          options: ["1", "2"],
+          options: ['1', '2'],
           __v: 0
         });
       });
   });
 
-  it('DELETES polls vea DELETE', () => {
-    return Poll.create(
+  it('DELETES polls vea DELETE', async() => {
+    const poll = await Poll.create(
       {
         organization: organization._id,
         title: 'the poll',
         description: 'its a pole',
         options: [1, 2]
-      })
-      .then(poll => request(app).delete(`/api/v1/polls/${poll.id}`))
+      });
+
+    return agent.delete(`/api/v1/polls/${poll.id}`)
       .then(res => {
         expect(res.body).toEqual(
           {
